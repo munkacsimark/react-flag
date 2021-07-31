8e79460520eb0a4a89f5614d381f4a5100ab36df
Mark Munkacsi <markmunkacsi@protonmail.com>
2021-07-31 18:14:45 +0400

    Working flag images

--- src/Flag.tsx
+++ src/Flag.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import {
+import Emoji from 'a11y-react-emoji'
+import Country, {
 	Alpha2CountryCode,
 	Alpha3CountryCode,
 	CountryFlagEmoji,
@@ -7,25 +8,71 @@ import {
 	NumericCountryCode,
 } from './Country'
 import countryData from './countryData'
+import flags from './flags'
+
 interface Props extends React.HTMLAttributes<HTMLElement> {
 	alpha2?: Alpha2CountryCode
 	alpha3?: Alpha3CountryCode
 	numeric?: NumericCountryCode
 	tld?: CountryTLD
+	useEmoji?: boolean
+	disableLabel?: boolean
 }
 
-const atLeastOnePropDefined = (props: Props) => {
-	const { alpha2, alpha3, numeric, tld } = props
-	return (
-		alpha2 !== undefined ||
-		alpha3 !== undefined ||
-		numeric !== undefined ||
-		tld !== undefined
-	)
+const atLeastOnePropDefined = ({
+	alpha2,
+	alpha3,
+	numeric,
+	tld,
+}: Props): boolean =>
+	alpha2 !== undefined ||
+	alpha3 !== undefined ||
+	numeric !== undefined ||
+	tld !== undefined
+
+const getCountryDataByProps = ({
+	alpha2,
+	alpha3,
+	numeric,
+	tld,
+}: Props): Country => {
+	if (!!alpha2) {
+		const data = countryData.find((data) => data.alpha2 === alpha2)
+		if (!data) throw Error(`Invalid aplha-2 code: "${alpha2}"`)
+		return data
+	}
+	if (!!alpha3) {
+		const data = countryData.find((data) => data.alpha3 === alpha3)
+		if (!data) throw Error(`Invalid aplha-3 code: "${alpha3}"`)
+		return data
+	}
+	if (!!numeric) {
+		const data = countryData.find((data) => data.numeric === numeric)
+		if (!data) throw Error(`Invalid numeric code: "${numeric}"`)
+		return data
+	}
+	if (!!tld) {
+		const data = countryData.find((data) => data.tld === tld)
+		if (!data) throw Error(`Invalid tld: "${tld}"`)
+		return data
+	}
+	throw Error("County don't exists with given props.")
 }
 
 const Flag: React.FunctionComponent<Props> = (props) => {
-	const flag = <span {...props}></span>
+	const { alpha2, alpha3, numeric, tld, useEmoji, disableLabel } = props
+	const countryData = getCountryDataByProps({ alpha2, alpha3, numeric, tld })
+
+	const flag = useEmoji ? (
+		<Emoji
+			symbol={countryData.emoji}
+			label={disableLabel ? undefined : `Flag of ${countryData.name}`}
+		/>
+	) : (
+		<span>
+			<img src={flags[`flag${alpha2}`]} alt={`Flag of ${countryData.name}`} />
+		</span>
+	)
 	return atLeastOnePropDefined(props) ? flag : null
 }
 
---
+++ src/flags.ts
@@ -0,0 +1,495 @@
+const flagAF = require('../flags/AF.svg').default
+const flagAX = require('../flags/AX.svg').default
+const flagAL = require('../flags/AL.svg').default
+const flagDZ = require('../flags/DZ.svg').default
+const flagAS = require('../flags/AS.svg').default
+const flagAD = require('../flags/AD.svg').default
+const flagAO = require('../flags/AO.svg').default
+const flagAI = require('../flags/AI.svg').default
+const flagAQ = require('../flags/AQ.svg').default
+const flagAG = require('../flags/AG.svg').default
+const flagAR = require('../flags/AR.svg').default
+const flagAM = require('../flags/AM.svg').default
+const flagAW = require('../flags/AW.svg').default
+const flagAU = require('../flags/AU.svg').default
+const flagAT = require('../flags/AT.svg').default
+const flagAZ = require('../flags/AZ.svg').default
+const flagBS = require('../flags/BS.svg').default
+const flagBH = require('../flags/BH.svg').default
+const flagBD = require('../flags/BD.svg').default
+const flagBB = require('../flags/BB.svg').default
+const flagBY = require('../flags/BY.svg').default
+const flagBE = require('../flags/BE.svg').default
+const flagBZ = require('../flags/BZ.svg').default
+const flagBJ = require('../flags/BJ.svg').default
+const flagBM = require('../flags/BM.svg').default
+const flagBT = require('../flags/BT.svg').default
+const flagBO = require('../flags/BO.svg').default
+const flagBA = require('../flags/BA.svg').default
+const flagBW = require('../flags/BW.svg').default
+const flagBR = require('../flags/BR.svg').default
+const flagIO = require('../flags/IO.svg').default
+const flagVG = require('../flags/VG.svg').default
+const flagBN = require('../flags/BN.svg').default
+const flagBG = require('../flags/BG.svg').default
+const flagBF = require('../flags/BF.svg').default
+const flagBI = require('../flags/BI.svg').default
+const flagCV = require('../flags/CV.svg').default
+const flagKH = require('../flags/KH.svg').default
+const flagCM = require('../flags/CM.svg').default
+const flagCA = require('../flags/CA.svg').default
+const flagKY = require('../flags/KY.svg').default
+const flagCF = require('../flags/CF.svg').default
+const flagTD = require('../flags/TD.svg').default
+const flagCL = require('../flags/CL.svg').default
+const flagCN = require('../flags/CN.svg').default
+const flagCX = require('../flags/CX.svg').default
+const flagCC = require('../flags/CC.svg').default
+const flagCO = require('../flags/CO.svg').default
+const flagKM = require('../flags/KM.svg').default
+const flagCD = require('../flags/CD.svg').default
+const flagCG = require('../flags/CG.svg').default
+const flagCK = require('../flags/CK.svg').default
+const flagCR = require('../flags/CR.svg').default
+const flagCI = require('../flags/CI.svg').default
+const flagHR = require('../flags/HR.svg').default
+const flagCU = require('../flags/CU.svg').default
+const flagCW = require('../flags/CW.svg').default
+const flagCY = require('../flags/CY.svg').default
+const flagCZ = require('../flags/CZ.svg').default
+const flagDK = require('../flags/DK.svg').default
+const flagDJ = require('../flags/DJ.svg').default
+const flagDM = require('../flags/DM.svg').default
+const flagDO = require('../flags/DO.svg').default
+const flagEC = require('../flags/EC.svg').default
+const flagEG = require('../flags/EG.svg').default
+const flagSV = require('../flags/SV.svg').default
+const flagGQ = require('../flags/GQ.svg').default
+const flagER = require('../flags/ER.svg').default
+const flagEE = require('../flags/EE.svg').default
+const flagSZ = require('../flags/SZ.svg').default
+const flagET = require('../flags/ET.svg').default
+const flagFK = require('../flags/FK.svg').default
+const flagFO = require('../flags/FO.svg').default
+const flagFJ = require('../flags/FJ.svg').default
+const flagFI = require('../flags/FI.svg').default
+const flagFR = require('../flags/FR.svg').default
+const flagGF = require('../flags/GF.svg').default
+const flagPF = require('../flags/PF.svg').default
+const flagTF = require('../flags/TF.svg').default
+const flagGA = require('../flags/GA.svg').default
+const flagGM = require('../flags/GM.svg').default
+const flagGE = require('../flags/GE.svg').default
+const flagDE = require('../flags/DE.svg').default
+const flagGH = require('../flags/GH.svg').default
+const flagGI = require('../flags/GI.svg').default
+const flagGR = require('../flags/GR.svg').default
+const flagGL = require('../flags/GL.svg').default
+const flagGD = require('../flags/GD.svg').default
+const flagGP = require('../flags/GP.svg').default
+const flagGU = require('../flags/GU.svg').default
+const flagGT = require('../flags/GT.svg').default
+const flagGG = require('../flags/GG.svg').default
+const flagGN = require('../flags/GN.svg').default
+const flagGW = require('../flags/GW.svg').default
+const flagGY = require('../flags/GY.svg').default
+const flagHT = require('../flags/HT.svg').default
+const flagHM = require('../flags/HM.svg').default
+const flagVA = require('../flags/VA.svg').default
+const flagHN = require('../flags/HN.svg').default
+const flagHK = require('../flags/HK.svg').default
+const flagHU = require('../flags/HU.svg').default
+const flagIS = require('../flags/IS.svg').default
+const flagIN = require('../flags/IN.svg').default
+const flagID = require('../flags/ID.svg').default
+const flagIR = require('../flags/IR.svg').default
+const flagIQ = require('../flags/IQ.svg').default
+const flagIE = require('../flags/IE.svg').default
+const flagIM = require('../flags/IM.svg').default
+const flagIL = require('../flags/IL.svg').default
+const flagIT = require('../flags/IT.svg').default
+const flagJM = require('../flags/JM.svg').default
+const flagJP = require('../flags/JP.svg').default
+const flagJE = require('../flags/JE.svg').default
+const flagJO = require('../flags/JO.svg').default
+const flagKZ = require('../flags/KZ.svg').default
+const flagKE = require('../flags/KE.svg').default
+const flagKI = require('../flags/KI.svg').default
+const flagKP = require('../flags/KP.svg').default
+const flagKR = require('../flags/KR.svg').default
+const flagKW = require('../flags/KW.svg').default
+const flagKG = require('../flags/KG.svg').default
+const flagLA = require('../flags/LA.svg').default
+const flagLV = require('../flags/LV.svg').default
+const flagLB = require('../flags/LB.svg').default
+const flagLS = require('../flags/LS.svg').default
+const flagLR = require('../flags/LR.svg').default
+const flagLY = require('../flags/LY.svg').default
+const flagLI = require('../flags/LI.svg').default
+const flagLT = require('../flags/LT.svg').default
+const flagLU = require('../flags/LU.svg').default
+const flagMO = require('../flags/MO.svg').default
+const flagMK = require('../flags/MK.svg').default
+const flagMG = require('../flags/MG.svg').default
+const flagMW = require('../flags/MW.svg').default
+const flagMY = require('../flags/MY.svg').default
+const flagMV = require('../flags/MV.svg').default
+const flagML = require('../flags/ML.svg').default
+const flagMT = require('../flags/MT.svg').default
+const flagMH = require('../flags/MH.svg').default
+const flagMQ = require('../flags/MQ.svg').default
+const flagMR = require('../flags/MR.svg').default
+const flagMU = require('../flags/MU.svg').default
+const flagYT = require('../flags/YT.svg').default
+const flagMX = require('../flags/MX.svg').default
+const flagFM = require('../flags/FM.svg').default
+const flagMD = require('../flags/MD.svg').default
+const flagMC = require('../flags/MC.svg').default
+const flagMN = require('../flags/MN.svg').default
+const flagME = require('../flags/ME.svg').default
+const flagMS = require('../flags/MS.svg').default
+const flagMA = require('../flags/MA.svg').default
+const flagMZ = require('../flags/MZ.svg').default
+const flagMM = require('../flags/MM.svg').default
+const flagNA = require('../flags/NA.svg').default
+const flagNR = require('../flags/NR.svg').default
+const flagNP = require('../flags/NP.svg').default
+const flagNL = require('../flags/NL.svg').default
+const flagNC = require('../flags/NC.svg').default
+const flagNZ = require('../flags/NZ.svg').default
+const flagNI = require('../flags/NI.svg').default
+const flagNE = require('../flags/NE.svg').default
+const flagNG = require('../flags/NG.svg').default
+const flagNU = require('../flags/NU.svg').default
+const flagNF = require('../flags/NF.svg').default
+const flagMP = require('../flags/MP.svg').default
+const flagNO = require('../flags/NO.svg').default
+const flagOM = require('../flags/OM.svg').default
+const flagPK = require('../flags/PK.svg').default
+const flagPW = require('../flags/PW.svg').default
+const flagPS = require('../flags/PS.svg').default
+const flagPA = require('../flags/PA.svg').default
+const flagPG = require('../flags/PG.svg').default
+const flagPY = require('../flags/PY.svg').default
+const flagPE = require('../flags/PE.svg').default
+const flagPH = require('../flags/PH.svg').default
+const flagPN = require('../flags/PN.svg').default
+const flagPL = require('../flags/PL.svg').default
+const flagPT = require('../flags/PT.svg').default
+const flagPR = require('../flags/PR.svg').default
+const flagQA = require('../flags/QA.svg').default
+const flagRE = require('../flags/RE.svg').default
+const flagRO = require('../flags/RO.svg').default
+const flagRU = require('../flags/RU.svg').default
+const flagRW = require('../flags/RW.svg').default
+const flagBL = require('../flags/BL.svg').default
+const flagSH = require('../flags/SH.svg').default
+const flagKN = require('../flags/KN.svg').default
+const flagLC = require('../flags/LC.svg').default
+const flagMF = require('../flags/MF.svg').default
+const flagPM = require('../flags/PM.svg').default
+const flagVC = require('../flags/VC.svg').default
+const flagWS = require('../flags/WS.svg').default
+const flagSM = require('../flags/SM.svg').default
+const flagST = require('../flags/ST.svg').default
+const flagSA = require('../flags/SA.svg').default
+const flagSN = require('../flags/SN.svg').default
+const flagRS = require('../flags/RS.svg').default
+const flagSC = require('../flags/SC.svg').default
+const flagSL = require('../flags/SL.svg').default
+const flagSG = require('../flags/SG.svg').default
+const flagSX = require('../flags/SX.svg').default
+const flagSK = require('../flags/SK.svg').default
+const flagSI = require('../flags/SI.svg').default
+const flagSB = require('../flags/SB.svg').default
+const flagSO = require('../flags/SO.svg').default
+const flagZA = require('../flags/ZA.svg').default
+const flagGS = require('../flags/GS.svg').default
+const flagSS = require('../flags/SS.svg').default
+const flagES = require('../flags/ES.svg').default
+const flagLK = require('../flags/LK.svg').default
+const flagSD = require('../flags/SD.svg').default
+const flagSR = require('../flags/SR.svg').default
+const flagSE = require('../flags/SE.svg').default
+const flagCH = require('../flags/CH.svg').default
+const flagSY = require('../flags/SY.svg').default
+const flagTW = require('../flags/TW.svg').default
+const flagTJ = require('../flags/TJ.svg').default
+const flagTZ = require('../flags/TZ.svg').default
+const flagTH = require('../flags/TH.svg').default
+const flagTL = require('../flags/TL.svg').default
+const flagTG = require('../flags/TG.svg').default
+const flagTK = require('../flags/TK.svg').default
+const flagTO = require('../flags/TO.svg').default
+const flagTT = require('../flags/TT.svg').default
+const flagTN = require('../flags/TN.svg').default
+const flagTR = require('../flags/TR.svg').default
+const flagTM = require('../flags/TM.svg').default
+const flagTC = require('../flags/TC.svg').default
+const flagTV = require('../flags/TV.svg').default
+const flagUG = require('../flags/UG.svg').default
+const flagUA = require('../flags/UA.svg').default
+const flagAE = require('../flags/AE.svg').default
+const flagGB = require('../flags/GB.svg').default
+const flagUS = require('../flags/US.svg').default
+const flagUY = require('../flags/UY.svg').default
+const flagUZ = require('../flags/UZ.svg').default
+const flagVU = require('../flags/VU.svg').default
+const flagVE = require('../flags/VE.svg').default
+const flagVN = require('../flags/VN.svg').default
+const flagVI = require('../flags/VI.svg').default
+const flagWF = require('../flags/WF.svg').default
+const flagEH = require('../flags/EH.svg').default
+const flagYE = require('../flags/YE.svg').default
+const flagZM = require('../flags/ZM.svg').default
+const flagZW = require('../flags/ZW.svg').default
+
+const flags: { [key: string]: string } = {
+	flagAF,
+	flagAX,
+	flagAL,
+	flagDZ,
+	flagAS,
+	flagAD,
+	flagAO,
+	flagAI,
+	flagAQ,
+	flagAG,
+	flagAR,
+	flagAM,
+	flagAW,
+	flagAU,
+	flagAT,
+	flagAZ,
+	flagBS,
+	flagBH,
+	flagBD,
+	flagBB,
+	flagBY,
+	flagBE,
+	flagBZ,
+	flagBJ,
+	flagBM,
+	flagBT,
+	flagBO,
+	flagBA,
+	flagBW,
+	flagBR,
+	flagIO,
+	flagVG,
+	flagBN,
+	flagBG,
+	flagBF,
+	flagBI,
+	flagCV,
+	flagKH,
+	flagCM,
+	flagCA,
+	flagKY,
+	flagCF,
+	flagTD,
+	flagCL,
+	flagCN,
+	flagCX,
+	flagCC,
+	flagCO,
+	flagKM,
+	flagCD,
+	flagCG,
+	flagCK,
+	flagCR,
+	flagCI,
+	flagHR,
+	flagCU,
+	flagCW,
+	flagCY,
+	flagCZ,
+	flagDK,
+	flagDJ,
+	flagDM,
+	flagDO,
+	flagEC,
+	flagEG,
+	flagSV,
+	flagGQ,
+	flagER,
+	flagEE,
+	flagSZ,
+	flagET,
+	flagFK,
+	flagFO,
+	flagFJ,
+	flagFI,
+	flagFR,
+	flagGF,
+	flagPF,
+	flagTF,
+	flagGA,
+	flagGM,
+	flagGE,
+	flagDE,
+	flagGH,
+	flagGI,
+	flagGR,
+	flagGL,
+	flagGD,
+	flagGP,
+	flagGU,
+	flagGT,
+	flagGG,
+	flagGN,
+	flagGW,
+	flagGY,
+	flagHT,
+	flagHM,
+	flagVA,
+	flagHN,
+	flagHK,
+	flagHU,
+	flagIS,
+	flagIN,
+	flagID,
+	flagIR,
+	flagIQ,
+	flagIE,
+	flagIM,
+	flagIL,
+	flagIT,
+	flagJM,
+	flagJP,
+	flagJE,
+	flagJO,
+	flagKZ,
+	flagKE,
+	flagKI,
+	flagKP,
+	flagKR,
+	flagKW,
+	flagKG,
+	flagLA,
+	flagLV,
+	flagLB,
+	flagLS,
+	flagLR,
+	flagLY,
+	flagLI,
+	flagLT,
+	flagLU,
+	flagMO,
+	flagMK,
+	flagMG,
+	flagMW,
+	flagMY,
+	flagMV,
+	flagML,
+	flagMT,
+	flagMH,
+	flagMQ,
+	flagMR,
+	flagMU,
+	flagYT,
+	flagMX,
+	flagFM,
+	flagMD,
+	flagMC,
+	flagMN,
+	flagME,
+	flagMS,
+	flagMA,
+	flagMZ,
+	flagMM,
+	flagNA,
+	flagNR,
+	flagNP,
+	flagNL,
+	flagNC,
+	flagNZ,
+	flagNI,
+	flagNE,
+	flagNG,
+	flagNU,
+	flagNF,
+	flagMP,
+	flagNO,
+	flagOM,
+	flagPK,
+	flagPW,
+	flagPS,
+	flagPA,
+	flagPG,
+	flagPY,
+	flagPE,
+	flagPH,
+	flagPN,
+	flagPL,
+	flagPT,
+	flagPR,
+	flagQA,
+	flagRE,
+	flagRO,
+	flagRU,
+	flagRW,
+	flagBL,
+	flagSH,
+	flagKN,
+	flagLC,
+	flagMF,
+	flagPM,
+	flagVC,
+	flagWS,
+	flagSM,
+	flagST,
+	flagSA,
+	flagSN,
+	flagRS,
+	flagSC,
+	flagSL,
+	flagSG,
+	flagSX,
+	flagSK,
+	flagSI,
+	flagSB,
+	flagSO,
+	flagZA,
+	flagGS,
+	flagSS,
+	flagES,
+	flagLK,
+	flagSD,
+	flagSR,
+	flagSE,
+	flagCH,
+	flagSY,
+	flagTW,
+	flagTJ,
+	flagTZ,
+	flagTH,
+	flagTL,
+	flagTG,
+	flagTK,
+	flagTO,
+	flagTT,
+	flagTN,
+	flagTR,
+	flagTM,
+	flagTC,
+	flagTV,
+	flagUG,
+	flagUA,
+	flagAE,
+	flagGB,
+	flagUS,
+	flagUY,
+	flagUZ,
+	flagVU,
+	flagVE,
+	flagVN,
+	flagVI,
+	flagWF,
+	flagEH,
+	flagYE,
+	flagZM,
+	flagZW,
+}
+
+export default flags
